f661c5afdd0fdad1dfcd126c663b1265aac0ae0b
Namra Patel <namrajpatel@gmail.com>
2019-04-23 21:45:17 -0400

    Initial firebase db connection
    Connected the newly made firebase db on console to the app files.

--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -6,8 +6,8 @@ class Colors {
 
   const Colors();
 
-  static const Color loginGradientStart = const Color(0xFFfbab66);
-  static const Color loginGradientEnd = const Color(0xFFf7418c);
+  static const Color loginGradientStart = const Color(0xFFC5CAE9);
+  static const Color loginGradientEnd = const Color(0xFF283593);
 
   static const primaryGradient = const LinearGradient(
     colors: const [loginGradientStart, loginGradientEnd],
