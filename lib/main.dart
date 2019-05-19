16d088de898c20e80924521db245cc3af8e0d801
Namra Patel <namrajpatel@gmail.com>
2019-05-18 20:52:58 -0400

    Restarting.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:funder/login_screen.dart';
+
 
 void main() => runApp(new MyApp());
 
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: new LoginPage(),
+      
     );
   }
 }
