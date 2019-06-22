9f6b3ab19365f47d2ebce06ea7a5217b2ef42fbe
shahab <shahab.salem6@gmail.com>
2019-06-22 00:46:16 -0600

    Update signuppage.dart

--- lib/signuppage.dart
+++ lib/signuppage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:funder/loginpage.dart';
-
-//services
+import 'services/usermanagement.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class SignupPage extends StatefulWidget {
@@ -10,169 +8,65 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
-  String _email="", _password='', _password2='', _errorMessage;
+  String _email, _password;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  
-    @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
-          body: Form(
-            key: _formKey,
-            child: Container(
-                padding: EdgeInsets.all(25.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    getUserEmail(),
-                    getUserPassword(),
-                    getUserPassword2(),
-                    signUpButton(),
-                    printError(),
-                    printDNM(),
-                    Text(_email),
-  
-                    //SizedBox(height: 15.0),
-                    
-                    //SizedBox(height: 20.0),
-                    
-                        //FirebaseAuth.instance
-                          //  .createUserWithEmailAndPassword(email: _email, password: _password)
-                            //then((signedInUser) {
-                          //UserManagement().storeNewUser(signedInUser, context);
-                        //}).catchError((e) {
-                       //  print(e);
-                       // });
-                  
-                   
-                  ],
-                )),
-      ));
-    }
-    Widget getUserEmail(){
-      return Padding( 
-      padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
-      child: TextFormField(
-          decoration: InputDecoration(hintText: 'Email'),
-          onSaved: (value) {
-            setState(() {
-              _email = value;
-            });
-            print(_email);
-          }),
-      );
-    }
-    Widget getUserPassword(){
-      return Padding( 
-      padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
-      child: TextFormField(
-        decoration: InputDecoration(hintText: 'Password'),
-            onSaved: (value) {
-              setState(() {
-                _password = value;
-                        });
-          }),
-      );
-    }
 
-    Widget getUserPassword2(){
-      return Padding( 
-      padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
-      child: TextFormField(
-        decoration: InputDecoration(hintText: 'Re-Enter Password'),
-            onSaved: (value) {
-              setState(() {
-                _password2 = value;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: Form(
+          key: _formKey,
+          child: Container(
+              padding: EdgeInsets.all(25.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                      validator: (input) => (input.isEmpty||!input.contains('@')) ? 'Please enter a valid email': null,
+                      decoration: InputDecoration(hintText: 'Email'),
+                      onSaved: (value) {
+                        setState(() {
+                          _email = value;
                         });
-          }),
-      );
-    }
-    Widget signUpButton(){
-        return new RaisedButton(
-          child: Text('Create Account'),
-          onPressed: _showDialog, 
-          color: Colors.blue,
-          textColor: Colors.white,
-          elevation: 7.0,
-          
-        );
-    }
-  
-    Widget printDNM(){
-      if(_password != _password2){
-      return new Text(
-        "Passwords do not match",
-        style: TextStyle(
-            fontSize: 13.0,
-            color: Colors.red,
-            height: 1.0,
-            fontWeight: FontWeight.w300),
-      );
-    }else{
-      return new Container(
-        height: 0.0,
-      );
+                      }),
+                  SizedBox(height: 15.0),
+                  TextFormField(
+                      validator: (input) => (input.isEmpty||input.length<6) ? 'Your password needs at least 6 characters': null,
+                      decoration: InputDecoration(hintText: 'Password'),
+                      obscureText: true,
+                      onSaved: (value) {
+                        setState(() {
+                          _password = value;
+                        });
+                      }),
+                  SizedBox(height: 20.0),
+                  RaisedButton(
+                    child: Text('Sign Up'),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    elevation: 7.0,
+                    onPressed: createUser,
+                  ),
+                ],
+              )),
+        ));
+  }
+  void createUser() async{
+    final formState = _formKey.currentState;
+    if(formState.validate()){
+      formState.save();
+      try{
+        FirebaseUser user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email , password: _password)
+            .then((signedInUser){
+          UserManagement().storeNewUser(signedInUser, context);
+
+        });
+        user.sendEmailVerification();
+        Navigator.of(context).pop(); //closes the page
+      }catch(e){
+        print(e.message);
+
       }
     }
-
-    Widget printError(){
-    if (_errorMessage != null) {
-      print(_errorMessage +"ASFDSA");
-      return new Text(
-        _errorMessage,
-        style: TextStyle(
-            fontSize: 13.0,
-            color: Colors.red,
-            height: 1.0,
-            fontWeight: FontWeight.w300), 
-      );
-    } else {
-      return new Container(
-        height: 0.0,
-      );
-    }
   }
-    Future <void> createUser() async{
-      final formState = _formKey.currentState;
-     
-        if(formState.validate()){
-          print('user creatred');
-          formState.save();
-          try{
-            setState(() { _errorMessage = "";});
-             FirebaseUser user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email , password: _password);
-            user.sendEmailVerification();
-            Navigator.of(context).pop(); //closes the page
-            //Navigator.pushReplacment(context, MaterialPageRoute(builder: (contect) => HomePage()));
-            
-          }catch(e){ 
-            setState(() { _errorMessage = e.message;});
-            print("error");
-          }
-      }
-    }
-    void _showDialog() {
-    // flutter defined function
-    createUser();
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text("Account Created"),
-          content: new Text("A Verification Email Has Been Sent to " + _email),
-          actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text("Return to Login"),
-              onPressed: () {
-                 Navigator.push(context, MaterialPageRoute(builder: (contect) => LoginPage()));
-              },
-            ),
-          ],
-        );
-      }, 
-    );
-  }
-  }
-  
-  
+}
