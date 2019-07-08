0596f4a0bd6ac5f9a3505da96a963b9a543a67c6
dhruvbabalu2 <33795784+dhruvbabalu2@users.noreply.github.com>
2019-07-07 21:30:42 -0400

    Update payment.dart

--- lib/payment.dart
+++ lib/payment.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PaymentsPage extends StatefulWidget {
   @override
@@ -8,23 +9,31 @@ class PaymentsPage extends StatefulWidget {
 }
 
 class _PaymentsPageState extends State<PaymentsPage> {
+
+  final screenH = ScreenUtil.instance.setHeight;
+  final screenW = ScreenUtil.instance.setWidth;
+  final screenF = ScreenUtil.instance.setSp;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-              Container(
-              height: 650.0,
+          Container(
+              height: screenH(850),
               width: double.infinity,
             ),
-              Container(
-              height: 250.0,
+            Container(
+              height: screenH(350),
               width: double.infinity,
               color: Colors.blueAccent[700],
             ),
             Positioned(
-              top: 32,
-              left: 5,
+              top: (MediaQuery.of(context).size.height / 16),
+              left: (MediaQuery.of(context).size.width / 45),
+              //top: 32,
+              //left: 5,
               child: IconButton(
                 icon: Icon(Icons.close),
                 onPressed: () {
@@ -34,14 +43,17 @@ class _PaymentsPageState extends State<PaymentsPage> {
               ),
             ),
               Positioned(
-              top: 125.0,
-              left: 15.0,
-              right: 15.0,
+              top: (MediaQuery.of(context).size.height / 5.7),
+              left: (MediaQuery.of(context).size.width / 21),
+              right: (MediaQuery.of(context).size.width / 21),
+              // top: 125.0,
+              // left: 15.0,
+              // right: 15.0,
               child: Material(
                 elevation: 3.0,
                 borderRadius: BorderRadius.circular(7.0),
                 child: Container(
-                  height: 250.0,
+                  height: screenH(330),
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(7.0),
                       color: Colors.white),
@@ -49,23 +61,25 @@ class _PaymentsPageState extends State<PaymentsPage> {
               ),
             ),
             Positioned(
-              top: 190,
-              left: 90,
+              //top: 190,
+              top: (MediaQuery.of(context).size.height / 3.7),
+              //left: 90,
+              left: (MediaQuery.of(context).size.width / 4),
               child: Text("CAD \$500.00 ", style: TextStyle(fontSize: 35),),
             ),
               Positioned(
-              top: 50,
-              left: 110,
+              top: (MediaQuery.of(context).size.height / 9),
+              left: (MediaQuery.of(context).size.width / 3.8),
               child: Text("Payment Methods", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),),
             ),
 
 
             Positioned(
-              top: 250,
-              left: 60,
+              top: (MediaQuery.of(context).size.height / 2.8),
+              left: (MediaQuery.of(context).size.width / 7),
               child: new SizedBox(
-              width: 300,
-              height: 50,
+              width: screenW(300),
+              height: screenH(75),
               child: new RaisedButton(
                 child: Text("Add Funds", style: TextStyle(color: Colors.white, fontSize: 20),),
                 color: Colors.blueAccent[700],
@@ -74,44 +88,42 @@ class _PaymentsPageState extends State<PaymentsPage> {
             )
             ),
               Positioned(
-              top: 150,
-              left: 50,
+              top: (MediaQuery.of(context).size.height / 4.8),
+              left: (MediaQuery.of(context).size.width / 8),
               child: Text("Dime Cash", style: TextStyle(fontSize: 15),),
             ),
               Positioned(
-              top: 400,
-              left: 30,
+              top: (MediaQuery.of(context).size.height / 1.72),
+              //left: 30,
+              left: (MediaQuery.of(context).size.width / 14),
               child: Text("Payment Methods", style: TextStyle(fontSize: 15),),
             ),
+
               Positioned(
-              top: 550,
-              left: 30,
-              child: Text("Promotions", style: TextStyle(fontSize: 15),),
-            ),
-              Positioned(
-              top: 310,
-              left: 10,
+              top: (MediaQuery.of(context).size.height / 2.2),
+              left: (MediaQuery.of(context).size.width / 29),
               child: new SizedBox(
-              width: 300,
-              height: 50,
+              width: screenW(300),
+              height: screenH(65),
               child: Icon(Icons.redeem)
             )
             ),
               Positioned(
-              top: 325,
-              left: 180,
+              top: (MediaQuery.of(context).size.height / 2.1),
+              left: (MediaQuery.of(context).size.width / 2.25),
               child: new SizedBox(
-              width: 300,
-              height: 50,
+              width: screenW(300),
+              height: screenH(65),
               child: Text("Redeem", style: TextStyle(fontSize: 20),)
             )
             ), 
             Positioned(
-              top: 425,
+              top: (MediaQuery.of(context).size.height / 1.61),
+              //top: 425,
               left: 0,
               child:Container(
                 color: Colors.white,
-                    width: 412,
+                    width: screenW(415),
                     child: ListTile(
                       onTap: (){},
                       title: Text("TD PERSONAL **** 1234",),
@@ -122,11 +134,11 @@ class _PaymentsPageState extends State<PaymentsPage> {
                   
             ),
               Positioned(
-              top: 480,
+              top: (MediaQuery.of(context).size.height / 1.45),
               left: 0,
               child:Container(
                 color: Colors.white,
-                    width: 412,
+                    width: screenW(415),
                     child: ListTile(
                       onTap: (){},
                       title: Text("Add Payment Method", style: TextStyle(color: Colors.blueAccent[700]),),
@@ -136,11 +148,11 @@ class _PaymentsPageState extends State<PaymentsPage> {
                   
             ),
               Positioned(
-              top: 575,
+              top: (MediaQuery.of(context).size.height / 1.199),
               left: 0,
               child:Container(
                 color: Colors.white,
-                    width: 412,
+                    width: screenW(415),
                     child: ListTile(
                       onTap: (){
                         _showBottom();
@@ -150,7 +162,12 @@ class _PaymentsPageState extends State<PaymentsPage> {
                       )
                   ),
                   
-            )
+            ),
+              Positioned(
+              top: (MediaQuery.of(context).size.height / 1.25),
+              left: (MediaQuery.of(context).size.width / 14),
+              child: Text("Promotions", style: TextStyle(fontSize: 15),),
+            ),
 
         ],
       ),
